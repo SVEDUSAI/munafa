728cad2bf7e8e809e1097096b905e4d0ee95e5e1
Asmita Hase <asmitahase@gmail.com>
2026-02-27 14:18:34 +0530

    fix: correct type hint

--- hrms/hr/doctype/attendance/attendance.py
+++ hrms/hr/doctype/attendance/attendance.py
@@ -368,7 +368,7 @@ def mark_bulk_attendance(data: str | dict):
 @frappe.whitelist()
 def get_unmarked_days(
 	employee: str, from_date: str | date, to_date: str | date, exclude_holidays: str | int = 0
-) -> int:
+) -> list:
 	joining_date, relieving_date = frappe.get_cached_value(
 		"Employee", employee, ["date_of_joining", "relieving_date"]
 	)
